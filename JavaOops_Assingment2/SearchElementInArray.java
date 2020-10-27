f5f85383dbce3c499d5269826b10caf16e80483a
Vaibhav Baraskar <54434875+VaibhavBaraskar@users.noreply.github.com>
2020-10-27 15:15:51 +0530

    Add files via upload

---
+++ JavaOops_Assingment2/SearchElementInArray.java
@@ -0,0 +1,35 @@
+package array_Basic;
+
+import java.util.Scanner;
+
+public class SearchElementInArray {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.print("How many number of elements do you want to insert := ");
+		int size=sc.nextInt();
+		int[] a=new int[size];
+		System.out.print("Enter the array elements := ");
+		for(int i=0;i<a.length;i++)
+		{
+			a[i]=sc.nextInt();
+		}
+		System.out.print("Enter the value that to be searchedin array :=");
+		int key=sc.nextInt();
+		int count=0;
+		for(int i=0;i<a.length;i++)
+		{
+			if(a[i]==key)
+			{
+				count++;
+				System.out.println("Element Found");
+			}
+			
+		}
+		
+		if(count==0) {
+			System.out.println("Element not found");
+		}	
+	}
+
+}
